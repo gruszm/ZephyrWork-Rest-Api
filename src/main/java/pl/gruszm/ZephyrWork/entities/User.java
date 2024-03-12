e299a9bb4ff4aff6b094f8da76c0ec9c5b3f3206
gruszm <mariuszgruszczynski11@gmail.com>
2024-03-12 22:30:04 +0100

    User entity created

---
+++ src/main/java/pl/gruszm/ZephyrWork/entities/User.java
@@ -0,0 +1,89 @@
+package pl.gruszm.ZephyrWork.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @OneToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.EAGER)
+    @JoinColumn(name = "supervisor_id", referencedColumnName = "id")
+    private User supervisor;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public User getSupervisor()
+    {
+        return supervisor;
+    }
+
+    public void setSupervisor(User supervisor)
+    {
+        this.supervisor = supervisor;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+}
