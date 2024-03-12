4177864d200427a65a6354911912f3e64500d312
gruszm <mariuszgruszczynski11@gmail.com>
2024-03-12 21:35:39 +0100

    Initial commit

---
+++ src/main/java/pl/gruszm/ZephyrWork/ZephyrWorkApplication.java
@@ -0,0 +1,13 @@
+package pl.gruszm.ZephyrWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZephyrWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZephyrWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/gruszm/ZephyrWork/ZephyrWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gruszm.ZephyrWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZephyrWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
