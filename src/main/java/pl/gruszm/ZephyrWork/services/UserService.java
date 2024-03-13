35c3bde79b5848afb569e14a9ffd9c9781dbdac1
gruszm <mariuszgruszczynski11@gmail.com>
2024-03-13 19:52:32 +0100

    User Service created

---
+++ src/main/java/pl/gruszm/ZephyrWork/services/UserService.java
@@ -0,0 +1,55 @@
+package pl.gruszm.ZephyrWork.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.gruszm.ZephyrWork.entities.User;
+import pl.gruszm.ZephyrWork.repostitories.UserRepository;
+
+import java.util.Optional;
+
+@Service
+public class UserService
+{
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+
+    public User findById(int id)
+    {
+        Optional<User> userOptional = userRepository.findById(id);
+
+        return userOptional.isPresent() ? userOptional.get() : null;
+    }
+
+    public User findByEmail(String email)
+    {
+        return userRepository.findByEmail(email);
+    }
+
+    public User save(User user)
+    {
+        User savedUser = userRepository.save(user);
+
+        return savedUser;
+    }
+
+    public User deleteUserById(int id)
+    {
+        Optional<User> userOptional = userRepository.findById(id);
+
+        if (userOptional.isPresent())
+        {
+            userRepository.deleteById(id);
+
+            return userOptional.get();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
