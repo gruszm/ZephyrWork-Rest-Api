b31ce5b8ab3bfe8abb7fd1d1cd69d8be0c525b53
gruszm <mariuszgruszczynski11@gmail.com>
2024-03-21 21:07:24 +0100

    Endpoint for token validation

--- src/main/java/pl/gruszm/ZephyrWork/security/SecurityController.java
+++ src/main/java/pl/gruszm/ZephyrWork/security/SecurityController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.gruszm.ZephyrWork.entities.User;
 import pl.gruszm.ZephyrWork.services.UserService;
 
@@ -53,6 +50,23 @@ public class SecurityController
             return ResponseEntity
                     .status(HttpStatus.UNAUTHORIZED)
                     .body(null);
+        }
+    }
+
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validateToken(@RequestHeader("Auth") String jwt)
+    {
+        UserDetails userDetails = jwtUtils.readToken(jwt);
+
+        if (userDetails == null)
+        {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .build();
+        }
+        else
+        {
+            return ResponseEntity.ok(null);
         }
     }
 }
